a41e6ebe9f63cd0558c734e969003276d61ceb97
MayankGEHU <minku6195@gmail.com>
2025-05-25 13:34:13 +0530

    all done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
-import { BackgroundBeamsWithCollision } from "./components/ui/background-beams-with-collision";
-// import { TracingBeamDemo } from "./components/ui/TracingBeamDemo";
-import {CardDemo} from "./components/ui/CardDemo"
 import { TimelineDemo } from "./components/ui/TimelineDemo";
-import { TabsDemo } from "./components/ui/TabsDemo";
-import { LayoutGridDemo } from "./components/ui/LayoutGridDemo";
 import { BackgroundBeamsWithCollisionDemo } from "./components/ui/BackgroundBeamsWithCollisionDemo";
-import { SparklesPreview } from "./components/ui/SparklesPreview";
 import { HeroHighlightDemo } from "./components/ui/HeroHighlightDemo";
 import { StickyScrollRevealDemo } from "./components/ui/StickyScrollRevealDemo";
 import { TextHoverEffectDemo } from "./components/ui/TextHoverEffectDemo";
@@ -14,16 +8,11 @@ export default function Home() {
   return (
     <div className="relative">
       <BackgroundBeamsWithCollisionDemo />
-      {/* <SparklesPreview/> */}
-      {/* <TracingBeamDemo/> */}
       <TimelineDemo/>
       <HeroHighlightDemo/>
-       {/* <LayoutGridDemo/> */}
        <TextHoverEffectDemo/>
        <StickyScrollRevealDemo/>
        <SignupFormDemo/>
-       {/* <TabsDemo/> */}
-        {/* <CardDemo/> */}
     </div>
   );
 }
