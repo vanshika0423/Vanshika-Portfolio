d789cc35faed697d969a449ae74ac8caf3759d77
MayankGEHU <minku6195@gmail.com>
2025-06-09 19:51:50 +0530

    codeforces image update

--- src/app/components/ui/TextHoverEffectDemo.jsx
+++ src/app/components/ui/TextHoverEffectDemo.jsx
@@ -3,8 +3,8 @@ import { TextHoverEffect } from "./text-hover-effect";
 
 export function TextHoverEffectDemo() {
   return (
-    <div className="mt-20 h-[15rem] flex items-center justify-center">
-      <TextHoverEffect text="Coding Platform" />
+    <div className="mt-20 h-[20rem] flex items-center justify-center">
+      <TextHoverEffect text="C P"/>
     </div>
   );
 }
