527570cf3f3b9c0bcb8b5005c340e2fc3f2499f1
MayankGEHU <minku6195@gmail.com>
2025-07-10 14:42:42 +0530

    hero section

--- src/app/components/ui/BackgroundBeamsWithCollisionDemo.jsx
+++ src/app/components/ui/BackgroundBeamsWithCollisionDemo.jsx
@@ -4,24 +4,28 @@ import { BackgroundBeamsWithCollision } from "./background-beams-with-collision"
 export function BackgroundBeamsWithCollisionDemo() {
   return (
     <BackgroundBeamsWithCollision>
-      <h2
-        className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-        Hi&apos; My Name is Mayank Singh{" "}
-        <div
-          className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-          <div
-            className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">A Competitive Coder.</span>
+      <div className="flex flex-col items-center justify-center h-screen w-full text-center px-4">
+        <h2 className="text-2xl md:text-4xl lg:text-7xl font-bold text-black dark:text-white font-sans tracking-tight z-20">
+          Hi&apos; My Name is Mayank Singh
+        </h2>
+
+        <div className="relative mt-4 z-20">
+          <div className="absolute left-0 top-0 w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+            <span className="text-2xl md:text-4xl lg:text-7xl font-bold">
+              A Competitive Coder & Frontend Developer.
+            </span>
           </div>
-          <div
-            className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">A Competitive Coder.</span>
+          <div className="relative bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">
+            <span className="text-2xl md:text-4xl lg:text-7xl font-bold">
+              A Competitive Coder & Frontend Developer.
+            </span>
           </div>
         </div>
-      </h2>
+      </div>
     </BackgroundBeamsWithCollision>
   );
 }
+
 
 // import React from "react";
 // import Spline from "@splinetool/react-spline";
