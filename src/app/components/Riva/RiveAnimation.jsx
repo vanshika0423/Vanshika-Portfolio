dd361dd7480dcdbb2af8fbcb6a922951bc8783c3
MayankGEHU <minku6195@gmail.com>
2025-07-17 14:43:18 +0530

    responsive

--- src/app/components/Riva/RiveAnimation.jsx
+++ src/app/components/Riva/RiveAnimation.jsx
@@ -5,13 +5,13 @@ import { useRive } from "@rive-app/react-canvas";
 export default function RiveAnimation() {
   const { RiveComponent } = useRive({
     src: "/rive/pomodoro.riv", 
-      stateMachines: "State Machine 1",
+    stateMachines: "State Machine 1",
     autoplay: true,
   });
 
-  return(
-      <div className="w-[600px] h-[600px]">
-        <RiveComponent className="w-full h-full" />
-      </div>
+  return (
+    <div className="w-[400px] h-[400px] md:w-[600px] md:h-[600px] mx-auto">
+      <RiveComponent className="w-full h-full" />
+    </div>
   );
 }
